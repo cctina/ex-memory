cb9be1e550ee015e726b7b88c7fe1660950658db
Yu-Chen Hsu <every2ever@gmail.com>
2016-05-31 00:22:44 -0700

    Add cover/arrows to each album (w/o func)

--- js/script.js
+++ js/script.js
@@ -1,22 +1,53 @@
 $(function() {
 	// check jquery imported or not
 	// $('header').append('<p>jQuery loaded successfully!<p>');	
+	$cover = $('#cover-tempalte');
 
 	// set hover animation on albums
-	$album = $(".album");
-	$album.hover(
+	$entry = $('.entry');
+	$entry.each(function() {
+		// create cover for each entry
+		$img = $(this).children('figure').children('img');
+		$thisCover = $cover.clone();
+		$thisCover.attr('id', $(this).attr('id') + 'Cover');
+		$thisCover.width($img.width());
+		$thisCover.height($img.height());
+		$thisCover.css('dispaly: inline-block');
+		// add cover to album
+		$img.before($thisCover);
+	});
+	$entry.hover(
 		//mouse enter
 		function(){
-		    $(this).animate({
+			$thisCover = $(this).children('figure').children('.cover');
+			$thisCover.animate({
 		    	opacity: 0.5
 		    }, 300);
 		},
 		//mouse leave 
 		function(){
-		    $(this).animate({
-		    	opacity: 1
+			$thisCover = $(this).children('figure').children('.cover');
+		    $thisCover.animate({
+		    	opacity: 0.0
 	    	}, 300);
 		}
-	); 
+	);
+
+	// set hover animation on albums
+	// $album = $(".album");
+	// $album.hover(
+	// 	//mouse enter
+	// 	function(){
+	// 		$(this).animate({
+	// 	    	opacity: 0.5
+	// 	    }, 300);
+	// 	},
+	// 	//mouse leave 
+	// 	function(){
+	// 	    $(this).animate({
+	// 	    	opacity: 1
+	//     	}, 300);
+	// 	}
+	// ); 
 });
 
